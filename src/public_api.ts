011f8c0d6a416b3acd83147b718db0f6c8f9e328
Rohit Khanduri <rohit.khanduri@hotmail.com>
2022-11-06 18:07:07 +0100

    [CHORE] packaging

---
+++ src/public_api.ts
@@ -0,0 +1,6 @@
+export * from './add-assets';
+export * from './add-dependency';
+export * from './add-service';
+export * from './bafa-initializer';
+export * from './prettify-and-lint';
+export * from './update-app-component';
