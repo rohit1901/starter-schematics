92dbda71c3e8aa252fe20ab3753361cb78377c11
Rohit Khanduri <rohit.khanduri@hotmail.com>
2022-11-07 12:40:39 +0100

    [FEATURE] ng new
    schematic now creates a new angular project

--- src/bafa-initializer/index.ts
+++ src/bafa-initializer/index.ts
@@ -1,14 +1,21 @@
 import { chain, schematic, Rule, externalSchematic } from "@angular-devkit/schematics";
 
 import { SchemaOptions } from "../types/schema-options/schema-options.interface";
+import { Schema } from "@schematics/angular/ng-new/schema";
 
 export default function(options: SchemaOptions): Rule {
   const dependencyOptions: SchemaOptions = {
     ...options, //TODO: replace lodash with `bafa-ui-library`
     dependencyName: "lodash"
   };
+  const ngNewOptions: Partial<Schema> = {
+    createApplication: true,
+    directory: './',
+    skipGit: true,
+    version: '14.2.8'
+  }
   return chain([
-    externalSchematic('@schematics/angular', 'ng-new', options),
+    externalSchematic('@schematics/angular', 'ng-new', ngNewOptions),
     schematic("update-app-component", options),
     schematic("add-dependency", dependencyOptions),
     schematic("prettify-and-lint", options),
